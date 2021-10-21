94bd0c3522e8edf45cb6e4975e074f14f78cbf4f
mfronistas <m.k.fronistas2@newcastle.ac.uk>
2021-10-21 13:09:33 +0100

    Added a logger to log in the operations of the user

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import socket
+import logging
+
+
+# logging
+class SecurityFilter(logging.Filter):
+    def filter(self, record):
+        return 'SECURITY' in record.getMessage()
+
+fh = logging.FileHandler('CSC2031blog.log', 'w')
+fh.setLevel(logging.WARNING)
+fh.addFilter(SecurityFilter())
+formatter = logging.Formatter('%(asctime)s : %(message)s', '%m/%d/%Y %I:%M:%S %p')
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('')
+logger.propagate = False
+logger.addHandler(fh)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LongAndRandomSecretKey'
@@ -18,14 +35,6 @@ db = SQLAlchemy(app)
 @app.route('/')
 def index():
     return render_template('index.html', name='Mike')
-
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-
-
 
 
 if __name__ == '__main__':
